0ac3a22882d747076350fa65777829b13efbeb6d
Lunatc <cintia.braz@unesp.br>
2023-01-24 19:01:36 -0300

    Update README

--- README.md
+++ README.md
@@ -7,7 +7,25 @@ Resolvi começar um novo portfólio do zero e apenas utilizando o básico de Fro
 
 <details>
 
-<summary> Janeiro 2023 </summary>
+<summary> To-Do </summary>
+
+- [ ] Escrever texto "Breve introdução"
+- [ ] Parte de projetos
+- [ ] Protótipos Figma
+- [ ] Parte com ferramentas e linguagens que sei
+- [ ] Adicionar icone window do navegador
+- [ ] Consertar visualização no celular (hr)
+- [ ] Escolher uma foto melhor
+- [ ] Inglês
+- [ ] Posters and Workshops
+- [ ] Modo escuro
+- [ ] Links font-awesome
+
+</details>
+
+<details> 
+
+<summary> Done </summary>
 
 - [x] Colocar uma foto minha
 - [x] Arrumar as cores
@@ -15,17 +33,8 @@ Resolvi começar um novo portfólio do zero e apenas utilizando o básico de Fro
 - [x] Icones redes sociais
 - [x] Link currículo
 - [x] Adicionar informações corretas sobre educação, experiência e outros
-- [ ] Parte de projetos
-- [ ] Protótipos Figma
-- [ ] Parte com ferramentas e linguagens que sei
 - [x] Trocar para React
 - [x] Github Pages
-- [ ] Adicionar icone window do navegador
-- [ ] Consertar visualização no celular (hr)
-- [ ] Escolher uma foto melhor
-- [ ] Inglês
-- [ ] Posters and Workshops
-- [ ] Modo escuro
 - [x] Decidir SideBar React
 
 </details>
--- portfolio/src/pages/about.js
+++ portfolio/src/pages/about.js
@@ -5,20 +5,8 @@ function About() {
     return (
         <div className={classes.About}>
             <div className={classes["about"]}>
-                {/*<img src="img/foto.jpg" alt="Foto da Cintia">*/}
-                <h1>Quem sou eu</h1>
+                <h1>Breve introdução</h1>
                 <p className={classes["small-text"]}>Estudante de Ciência da Computação, me especializando em UI/UX. Pretendo fazer um mestrado na área de IHC e trabalhar como UX Researcher ou Product Designer.</p>
-                <a className ={classes["curriculo"]} href="https://drive.google.com/file/d/14rvCWLhOxG_0czzwcFv7fVioEHtRPcnu/view?usp=sharing">Meu curriculo em PDF</a>
-                
-                {/*<ul className="redes">
-                    <li><a href="https://github.com/Lunatc"><i className="fa-brands fa-github-alt"></i></a></li>
-                    <li><a href="https://www.behance.net/cintiaBM"><i className="fa-brands fa-behance"></i></a></li>
-                    <li><a href="https://dribbble.com/CintiaBM"><i className="fa-brands fa-dribbble"></i></a></li>
-                    <li><a href="https://www.linkedin.com/in/cintia-braz/"><i className="fa-brands fa-linkedin-in"></i></a></li>
-                </ul>*/}
-
-                <h2>Contato:</h2>
-                <p className={classes["email"]}>cbrmesquita@gmail.com</p>
             </div>
         </div>
     );
