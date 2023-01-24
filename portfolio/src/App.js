82d2bbe33c082df11ad8724c9eccfbda02a23b02
Lunatc <cintia.braz@unesp.br>
2023-01-24 18:51:18 -0300

    Adicionei as páginas e o sidebar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,9 @@
 import { Route, Switch } from 'react-router-dom';
 
+import classes from "./App.css"
+
 import Header from "./layouts/header";
-//import Sidebar from './layouts/sidebar';
+import Sidebar from './layouts/sidebar';
 //import Footer from "./layouts/footer";
 import Projects from './pages/projects';
 import Main from './pages/main';
@@ -12,21 +14,24 @@ import Curriculum from './pages/curriculum';
 function App() {
   return <div>
     <Header/>
-    {/*<Sidebar/>*/}
-    <Switch>
-      <Route path='/'>
-        <Main/>
-      </Route>
-      <Route path='/sobre'>
-        <About/>
-      </Route>
-      <Route path='/curriculo'>
-        <Curriculum/>
-      </Route>
-      <Route path='/projetos'>
-        <Projects/>
-      </Route>
-    </Switch>
+    <div className={classes.App}>
+      <Sidebar/>
+      <Switch>
+        <Route path='/' exact>
+          <Main/>
+        </Route>
+        <Route path='/sobre'>
+          <About/>
+        </Route>
+        <Route path='/curriculo'>
+          <Curriculum/>
+        </Route>
+        <Route path='/projetos'>
+          <Projects/>
+        </Route>
+      </Switch>
+    </div>
+    
     {/*<Footer/>*/}
   </div>;
 }
--- portfolio/src/pages/main.js
+++ portfolio/src/pages/main.js
@@ -1,9 +1,12 @@
+import classes from './styles/main.module.css';
 
 function Main() {
     return (
-        <div>
-            <h1>Olá, eu sou a Cíntia</h1>
-            <p>Estudante de ciência da computação, com interesse especial em UI/UX</p>
+        <div className={classes.Main}>
+            <div className={classes["main-content"]}>
+                <h1>Olá, eu sou a Cíntia</h1>
+                <p>Estudante de ciência da computação, com interesse especial em UI/UX</p>
+            </div>
         </div>
     );
 }
