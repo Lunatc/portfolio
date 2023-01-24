9bec8f132d073930dc1a8a0b0682be8d31391b7f
Lunatc <cintia.braz@unesp.br>
2023-01-24 15:30:10 -0300

    Criação do projeto em React

---
+++ portfolio/src/App.js
@@ -0,0 +1,9 @@
+import Header from "./Layouts/header";
+
+function App() {
+  return (
+    <Header/>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Layouts/header.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom';
+import classes from './header.module.css';
+
+function Header() {
+    return (
+        <header className={classes.header}>
+            <h1>
+                
+            <Link to="/">Cíntia's Portfolio</Link>
+                
+            </h1>
+
+            <nav>
+                <ul>
+                     <li>
+                        <Link to="/sobre">Sobre</Link>
+                    </li>
+                    <li>
+                        <Link to="/curriculo">Currículo</Link>
+                    </li>
+                    <li>
+                        <Link to="/projetos">Projetos</Link>
+                    </li>
+                </ul>
+               
+            </nav>
+        </header>
+    );
+  }
+  
+export default Header;
